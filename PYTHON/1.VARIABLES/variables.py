8aad202cefbbb6aa8299af822ecf027ea36e64f6
audo-wrld <audo-wrld@kali>
2024-11-08 21:04:45 +0300

    Startup

---
+++ PYTHON/1.VARIABLES/variables.py
@@ -0,0 +1,8 @@
+name = "Debbie"
+age = 22
+city = "Siaya"
+working = False
+
+PIE = 3.142
+
+print(name, age, city, working)
---
+++ PYTHON/2.DATATYPES/all_datatypes.py
@@ -0,0 +1,45 @@
+# 1. Strings
+# 2. Numbers( Int, Complex, Float)
+# 3. Boolean
+# 4. Long
+# 5. Lists
+# 7. Tuples
+# 8. Dictionary
+import math
+
+
+#STRINGS
+city = "nair|obi"
+print(city)
+print(len(city))
+print(city[2])
+print(city[4:])
+
+firstName = "Debbie"
+secondName = "Njenga"
+print(firstName + " " + secondName)
+print(city.strip())
+print(city.split('|'))
+print(city.capitalize())
+
+#NUMBERS
+a = 10 
+b = 10.52
+# c = 4 + 2i
+print(a)
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+print(a**b)
+print(math.pow(a,b))
+print(math.sqrt(a))
+print(math.floor(b))
+print(math.ceil(b))
+
+#BOOLEAN
+isWorking = True
+print(isWorking)
+isWorking = False
+print(isWorking)
